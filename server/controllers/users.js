6d0e650062ed3f932a3f2170fa48db3f94730ce4
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-03-16 19:44:17 -0400

    Complete Designing Multi-Table Schema

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,3 +4,8 @@ module.exports = {
   get: function (req, res) {},
   post: function (req, res) {}
 };
+
+
+// http://g2pc1.bu.edu/~qzpeng/manual/MySQL%20Commands.htm
+
+
--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,12 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+    ID int NOT NULL,
+    userName varchar(255) NOT NULL,
+    messageText varchar(255),
+    gitHandle varchar(255),
+    roomName varChar(255),
+    PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -15,3 +21,5 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- insert into persons (ID, LastName, FirstName, Age) values (3, 'Akkaya', 'Cihad', 25);
+-- update persons set FirstName = 'Chuckles' where ID = 4;
